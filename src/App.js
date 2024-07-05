2b855c875e941ac9a38038f68fc19c54f25163c3
vaibhavD17 <vaibhav9723142179@gmail.com>
2024-07-05 09:38:35 +0530

    fruitabalesApp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import Headers from './components/Headers/Headers'
+import Footers from './components/Footers/Footers';
+import Home from './containers/Home/Home';
+import Contacts from './containers/Contacts/Contacts';
+import Shop from './containers/Shop/Shop';
+import ShopDetails from './containers/Shop-Details/ShopDetails';
+import Error from './containers/404Error/Error';
+import Cart from './containers/Cart/Cart';
+import Checkout from './containers/Checkout/Checkout';
+import Testimonial from './containers/Testimonial/Testimonial';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Headers />
+    {/* <Home /> */}
+    {/* <Contacts /> */}
+    {/* <Shop /> */}
+    {/* <ShopDetails /> */}
+    {/* <Error /> */}
+    {/* <Cart /> */}
+    {/* <Checkout /> */}
+    <Testimonial />
+    <Footers />
+    </>
   );
 }
 
